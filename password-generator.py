fe624e8686ee701b40b763fb33bdcd5e7badc33a
nik4nd <nik4nd@gmail.com>
2021-09-22 19:15:34 +0300

    Add password generator

---
+++ password-generator.py
@@ -0,0 +1,30 @@
+from random import choice
+
+num = int(input('\nEnter the number of passwords: '))
+while num <= 0:
+    print('\n| Enter a number greater than 0 |\n')
+    num = int(input('Enter the number of passwords: '))
+
+length = int(input('Enter the length of passwords: '))
+while length < 8 or length > 127:
+    print('\n| The minimum password length is 8 characters'
+          ' and the maximum is 127. |\n')
+    length = int(input('Enter the length of passwords: '))
+
+chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+all_passwords = set()
+while len(all_passwords) < num:
+    password = ''
+    for i in range(length):
+        password += choice(chars)
+    all_passwords.add(password)
+all_passwords = list(all_passwords)
+
+output = open('passwords.txt', 'w')
+for i in range(len(all_passwords)):
+    output.write(f'{i+1}) {all_passwords[i]}' + '\n')
+output.close()
+
+output = open('passwords.txt')
+print('\n' + output.read())
+output.close()
